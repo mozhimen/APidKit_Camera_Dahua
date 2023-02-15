b7f2c11a31d2109f292bf1c2a3c58fa12e251c8c
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2023-02-15 11:08:33 +0800

    0215

--- camerak_dahua/src/main/java/com/mozhimen/camerak_dahua/CameraKDahuaMgr.kt
+++ camerak_dahua/src/main/java/com/mozhimen/camerak_dahua/CameraKDahuaMgr.kt
@@ -2,8 +2,8 @@ package com.mozhimen.camerak_dahua
 
 import com.company.netsdk.common.NetSDKLib
 import com.mozhimen.basick.utilk.UtilKDate
-import com.mozhimen.basick.utilk.UtilKFile
 import com.mozhimen.basick.utilk.context.UtilKApplication
+import com.mozhimen.basick.utilk.file.UtilKFile
 import com.mozhimen.camerak_dahua.helpers.IPLoginHelper
 import com.mozhimen.camerak_dahua.helpers.LivePreviewHelper
 
